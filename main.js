fb1dc9d6d291870791220a17d483686eba8ce8b7
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-05-10 18:18:36 +0530

    back to default icon

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 miro.onReady(() => {
-  const icon =
-    '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="275.34570771242085 122.89937106918237 516 516" width="512" height="512"><defs><path d="M677.9 536.38C686.91 536.38 694.21 543.68 694.21 552.69C694.21 563.29 694.21 578.79 694.21 589.39C694.21 598.4 686.91 605.69 677.9 605.69C644.78 605.69 561.72 605.69 528.6 605.69C519.6 605.69 512.3 598.4 512.3 589.39C512.3 578.79 512.3 563.29 512.3 552.69C512.3 543.68 519.6 536.38 528.6 536.38C561.72 536.38 644.78 536.38 677.9 536.38Z" id="a2lv3H1RPA"></path><path d="M752.71 124.9L754.72 125.34L756.67 125.86L758.56 126.46L760.39 127.14L762.16 127.9L763.88 128.74L765.54 129.66L767.14 130.66L768.68 131.74L770.16 132.9L771.59 134.14L772.96 135.46L774.27 136.86L775.52 138.34L776.71 139.9L777.84 141.54L778.92 143.26L779.94 145.06L780.9 146.94L781.8 148.9L782.64 150.94L783.43 153.06L784.16 155.26L784.83 157.54L785.44 159.9L785.99 162.34L786.48 164.86L786.92 167.46L787.3 170.14L787.62 172.9L787.88 175.74L788.08 178.66L788.23 181.66L788.32 184.74L788.35 187.9L788.35 571.9L788.32 575.06L788.23 578.14L788.08 581.14L787.88 584.06L787.62 586.9L787.3 589.66L786.92 592.34L786.48 594.94L785.99 597.46L785.44 599.9L784.83 602.26L784.16 604.54L783.43 606.74L782.64 608.86L781.8 610.9L780.9 612.86L779.94 614.74L778.92 616.54L777.84 618.26L776.71 619.9L775.52 621.46L774.27 622.94L772.96 624.34L771.59 625.66L770.16 626.9L768.68 628.06L767.14 629.14L765.54 630.14L763.88 631.06L762.16 631.9L760.39 632.66L758.56 633.34L756.67 633.94L754.72 634.46L752.71 634.9L750.64 635.26L748.52 635.54L746.34 635.74L744.1 635.86L741.8 635.9L322.89 635.9L320.59 635.86L318.35 635.74L316.17 635.54L314.05 635.26L311.98 634.9L309.97 634.46L308.03 633.94L306.13 633.34L304.3 632.66L302.53 631.9L300.81 631.06L299.15 630.14L297.55 629.14L296.01 628.06L294.53 626.9L293.1 625.66L291.73 624.34L290.43 622.94L289.17 621.46L287.98 619.9L286.85 618.26L285.77 616.54L284.75 614.74L283.79 612.86L282.89 610.9L282.05 608.86L281.26 606.74L280.53 604.54L279.87 602.26L279.25 599.9L278.7 597.46L278.21 594.94L277.77 592.34L277.39 589.66L277.07 586.9L276.81 584.06L276.61 581.14L276.46 578.14L276.37 575.06L276.35 571.9L276.35 187.9L276.37 184.74L276.46 181.66L276.61 178.66L276.81 175.74L277.07 172.9L277.39 170.14L277.77 167.46L278.21 164.86L278.7 162.34L279.25 159.9L279.87 157.54L280.53 155.26L281.26 153.06L282.05 150.94L282.89 148.9L283.79 146.94L284.75 145.06L285.77 143.26L286.85 141.54L287.98 139.9L289.17 138.34L290.43 136.86L291.73 135.46L293.1 134.14L294.53 132.9L296.01 131.74L297.55 130.66L299.15 129.66L300.81 128.74L302.53 127.9L304.3 127.14L306.13 126.46L308.03 125.86L309.97 125.34L311.98 124.9L314.05 124.54L316.17 124.26L318.35 124.06L320.59 123.94L322.89 123.9L741.8 123.9L744.1 123.94L746.34 124.06L748.52 124.26L750.64 124.54L750.64 124.54L752.71 124.9Z" id="aU9VdDfV0"></path><clipPath id="clipa1jbNVOf1G"><use xlink:href="#aU9VdDfV0" opacity="1"></use></clipPath><path d="M742.81 129.29C767.96 129.29 788.35 149.67 788.35 174.82C788.35 241.53 788.35 396.1 788.35 462.8C788.35 487.95 767.96 508.34 742.81 508.34C650.31 508.34 418.34 508.34 325.83 508.34C300.68 508.34 280.3 487.95 280.3 462.8C280.3 396.1 280.3 241.53 280.3 174.82C280.3 149.67 300.68 129.29 325.83 129.29C418.34 129.29 650.31 129.29 742.81 129.29Z" id="aqXxbzPs"></path><path d="M476.31 536.38C485.31 536.38 492.61 543.68 492.61 552.69C492.61 563.29 492.61 578.79 492.61 589.39C492.61 598.4 485.31 605.69 476.31 605.69C443.19 605.69 360.13 605.69 327.01 605.69C318 605.69 310.7 598.4 310.7 589.39C310.7 578.79 310.7 563.29 310.7 552.69C310.7 543.68 318 536.38 327.01 536.38C360.13 536.38 443.19 536.38 476.31 536.38Z" id="a6hXCZNTd"></path></defs><g><g><use xlink:href="#a2lv3H1RPA" opacity="1" fill="#b9ecf5" fill-opacity="0.37"></use></g><g><use xlink:href="#aU9VdDfV0" opacity="1" fill="#73d5ec" fill-opacity="0"></use><g clip-path="url(#clipa1jbNVOf1G)"><use xlink:href="#aU9VdDfV0" opacity="1" fill-opacity="0" stroke="#9bcce9" stroke-width="20" stroke-opacity="1"></use></g></g><g><use xlink:href="#aqXxbzPs" opacity="1" fill="#b9ecf5" fill-opacity="0.29"></use><g><use xlink:href="#aqXxbzPs" opacity="1" fill-opacity="0" stroke="#22d3a6" stroke-width="0" stroke-opacity="1"></use></g></g><g><use xlink:href="#a6hXCZNTd" opacity="1" fill="#b9ecf5" fill-opacity="0.37"></use></g></g></svg>';
 
   miro.initialize({
     extensionPoints: {
       bottomBar: {
         title: "Generate Cards",
-        svgIcon: icon,
+        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
           // get selected widgets
@@ -36,7 +34,7 @@ miro.onReady(() => {
             let x = 0;
             let y = 100.0;
             let verticalItemCount = 0;
-            for (element of selectedWidgets) {
+            for (element of selectedShapes) {
               let c = await miro.board.widgets.create({
                 type: "card",
                 title: element.plainText,
@@ -59,7 +57,7 @@ miro.onReady(() => {
             // select the generated card to accomodate user's quick actions
             await miro.board.selection.selectWidgets(generatedCardsId);
             console.log(`Cardsy generated ${conversionShapeCount} cards for you.`);
-            miro.showNotification(`Generated ${conversionShapeCount} cards.`);
+            miro.showNotification(`Cardsy generated ${conversionShapeCount} cards.`);
             // zoom to the frame generated
             // create a frame and put all the generated card inside frame, if user has prompted
           }
