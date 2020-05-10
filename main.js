e1dc7156f4e74ab77321d8c65a911ee90f7a74bd
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-05-10 12:24:15 +0530

    generate cards, select and zoom to the selection

--- main.js
+++ main.js
@@ -2,38 +2,48 @@ miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
       bottomBar: {
-        title: "Sticker to shapes",
+        title: "Generate Card",
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
-          // Get selected widgets
+          // get selected widgets
           let selectedWidgets = await miro.board.selection.get();
 
-          // Filter stickers from selected widgets
-          let stickers = selectedWidgets.filter(
-            (widget) => widget.type === "STICKER"
-          );
-
-          // Delete selected stickers
-          await miro.board.widgets.deleteById(
-            stickers.map((sticker) => sticker.id)
-          );
-
-          // Create shapes from selected stickers
-          await miro.board.widgets.create(
-            stickers.map((sticker) => ({
-              type: "shape",
-              text: sticker.text,
-              x: sticker.x,
-              y: sticker.y,
-              width: sticker.bounds.width,
-              height: sticker.bounds.height,
-            }))
-          );
-
-          // Show success message
-          miro.showNotification("Stickers has been converted");
+          // if selection is empty, exit gracefully.
+          if (selectedWidgets.length == 0) {
+            miro.showNotification("Please select some shapes");
+          } else {
+            // create respective cards for selected widget
+            let generatedCards = [];
+            let x = 0;
+            let y = 100.0;
+            let verticalItemCount = 0;
+            for (element of selectedWidgets) {
+              let c = await miro.board.widgets.create({
+                type: "card",
+                title: element.plainText,
+                x: x,
+                y: y,
+              });
+              y = y + 100.0;
+              verticalItemCount++;
+              if (verticalItemCount >= 5) {
+                x = x + 350.0;
+                verticalItemCount = 0;
+                y = 100;
+              }
+              generatedCards.push(c[0]);
+            }
+            let generatedCardsId = [];
+            for(element of generatedCards){
+              generatedCardsId.push(element.id);
+            }
+            // select the generated card to accomodate user's quick actions
+            await miro.board.selection.selectWidgets(generatedCardsId);
+            // zoom to the first card generated
+            await miro.board.viewport.zoomToObject(generatedCards);
+          }
         },
       },
     },
