e32d083bece30b4070a002677dce6b6cba053bf4
Yash Bhalodi <yashbhalodi007@gmail.com>
2020-05-10 14:04:59 +0530

    typo fixed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 miro.onReady(() => {
   miro.initialize({
     extensionPoints: {
-      bottomBarwidgetContextMenu: {
+      widgetContextMenu: {
         title: "Generate Card",
         svgIcon:
           '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
